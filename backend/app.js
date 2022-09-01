f5f953625645d774e7b58a4e4ec51df794e72c05
reklama-expert@yandex.ru <reklama-expert@yandex.ru>
2022-09-01 15:55:11 +0300

    PR15_2.4

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+
 console.log(process.env.NODE_ENV);
 
 const cors = require('cors');
+
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -21,6 +23,12 @@ const { PORT = 3001 } = process.env;
 
 const app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 // подключаемся к серверу mongo
 async function main() {
